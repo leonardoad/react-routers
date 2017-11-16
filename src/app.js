57a43ae4ae6c93a268befb5631d9e4a4d3e5ba90
Leonardo <desenvolvimento@uneworld.com.br>
2017-11-16 18:14:27 -0200

    Adding "Courses" and "About" pages using Routes.Route.children

--- src/app.js
+++ src/app.js
@@ -4,10 +4,15 @@ import { Router, Route, hashHistory } from 'react-router';
 
 
 import App from './pages/App'
+import Courses from './pages/Courses'
+import About from './pages/About'
 
 ReactDOM.render((
 		<Router history={hashHistory}>
-			<Route path="/" component={App} />
+			<Route path="/" component={App} >
+				<Route path='/courses' component={Courses} />
+				<Route path='/about' component={About} />
+			</Route>
 		</Router>
 		),
         document.querySelector('[data-js="app"]')
--- src/pages/App.js
+++ src/pages/App.js
@@ -3,7 +3,24 @@ import React from 'react'
 export default class App extends React.Component {
     render(){
         return (
-        	  <h1>Showing components through Routers</h1>
+        	  <div>
+	        	  	<nav className="navbar ">
+						<div className="tabs">
+						  <ul>
+							    <li><a href="#/" className="navbar-item">Home</a> </li>
+							    <li><a href="#/courses" className="navbar-item">Courses</a> </li>
+								<li><a href="#/about" className="navbar-item">About</a> </li>
+						  </ul>
+						</div>
+					</nav>
+
+					<section className="section">
+						<div className="container">
+							{this.props.children}
+						</div>
+					</section>
+        	  </div>
+
         	);
     }
 } 
