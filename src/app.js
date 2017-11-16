4709a5dea5beaf011ed521a6b9888400c9a288fe
Leonardo <desenvolvimento@uneworld.com.br>
2017-11-16 18:29:56 -0200

    Adding IndexRoute from React Router

--- src/app.js
+++ src/app.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, hashHistory } from 'react-router';
+import { Router, Route, hashHistory, IndexRoute } from 'react-router';
 
 
 import App from './pages/App'
+import Home from './pages/Home'
 import Courses from './pages/Courses'
 import About from './pages/About'
 
 ReactDOM.render((
 		<Router history={hashHistory}>
 			<Route path="/" component={App} >
+				<IndexRoute component={Home} />
 				<Route path='/courses' component={Courses} />
 				<Route path='/about' component={About} />
 			</Route>
